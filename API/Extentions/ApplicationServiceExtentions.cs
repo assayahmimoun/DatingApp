07f4669cb0dde93c4ae247cca53e06dbcacbfc37
Assayah Mimoun <massayah@sqli.com>
2021-09-09 11:40:14 +0100

    End of section 8

--- API/Extentions/ApplicationServiceExtentions.cs
+++ API/Extentions/ApplicationServiceExtentions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using API.Services;
 using API.Interfaces;
+using API.Helpers;
 
 namespace API.Extentions
 {
@@ -12,6 +13,8 @@ namespace API.Extentions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
